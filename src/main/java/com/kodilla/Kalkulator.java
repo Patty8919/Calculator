d33e9ee4dd2514734015eb612f0ce1dc00c5ded8
Patty8919 <pat.maciejewska@gmail.com>
2019-06-01 19:58:48 +0200

    first commit

--- src/main/java/com/kodilla/Kalkulator.java
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -1,4 +1,25 @@
 package com.kodilla;
 
 public class Kalkulator {
+    public int addAToB(int a, int b)
+    {
+        return (a + b);
+
+    }
+
+    public double subtractDFromC(double c, double d)
+    {
+        return  c - d;
+    }
+
+
+    public static void main(String[] args) {
+        Kalkulator calculator = new Kalkulator();
+
+        double result = calculator.subtractDFromC(20.5, 3.2);
+        System.out.println(result);
+
+        int result2 = calculator.addAToB(25, 4);
+        System.out.println(result2);
+    }
 }
